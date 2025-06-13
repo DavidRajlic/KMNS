47d5e06704bf8f1cbf308454d65d4f1e8daed748
DavidRajlic <rajlic.david@gmail.com>
2025-06-14 00:59:30 +0200

    connection to database (mongoDB)

---
+++ backend/App.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const port = 4000;
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Povezava z MongoDB uspešna!");
+  })
+  .catch((err) => {
+    console.error("Napaka pri povezavi z MongoDB:", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`Strežnik posluša na http://localhost:${port}`);
+});
