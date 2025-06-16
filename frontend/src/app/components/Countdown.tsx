f324c31fb952046f5e8ae860c4e31c32cd72de36
DavidRajlic <rajlic.david@gmail.com>
2025-06-16 02:41:06 +0200

    created countdown

---
+++ frontend/src/app/components/Countdown.tsx
@@ -0,0 +1,37 @@
+// app/Countdown.tsx
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Countdown() {
+  const [countdown, setCountdown] = useState("");
+
+  useEffect(() => {
+    const targetDate = new Date("2025-06-27T10:00:00");
+    const interval = setInterval(() => {
+      const now = new Date();
+      const diff = targetDate.getTime() - now.getTime();
+
+      if (diff <= 0) {
+        setCountdown("Turnir se je začel!");
+        clearInterval(interval);
+        return;
+      }
+
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      setCountdown(`${days} dni, ${hours}h ${minutes}m ${seconds}s`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <p className="text-blue-600 font-medium mt-5">
+      Začetek turnirja čez: <span className="font-bold"> {countdown} </span>
+    </p>
+  );
+}
