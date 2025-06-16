ca066c1c7bc18a35f7d22b44a268bc7cda73f3d4
DavidRajlic <rajlic.david@gmail.com>
2025-06-17 00:12:39 +0200

    add path to rutes and added express.json

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const teamRoutes = require("./routes/TeamRoutes");
 const app = express();
 const port = 4000;
+
+app.use(express.json());
+app.use("/team", teamRoutes);
 
 // MongoDB connection
 mongoose
