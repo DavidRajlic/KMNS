6b454831d17eb7b58c341fc53788ded99058a26f
DavidRajlic <rajlic.david@gmail.com>
2025-06-18 03:38:56 +0200

    set default (0) for all number attributes in team model

--- backend/models/TeamModel.js
+++ backend/models/TeamModel.js
@@ -4,16 +4,16 @@ var Schema = mongoose.Schema;
 var TeamSchema = new Schema({
   name: String,
   group: String,
-  place: Number, // group place (1st, 2nd..)
-  points: Number,
-  goals_scored: Number,
-  goals_conceded: Number,
-  goals_diff: Number,
-  wins: Number,
-  draws: Number,
-  losses: Number,
-  isPlaying: Boolean,
-  matches_played: Number,
+  place: { type: Number, default: 0 }, // group place (1st, 2nd..)
+  points: { type: Number, default: 0 },
+  goals_scored: { type: Number, default: 0 },
+  goals_conceded: { type: Number, default: 0 },
+  goals_diff: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  draws: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  isPlaying: { type: Boolean, default: false },
+  matches_played: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("Team", TeamSchema);
