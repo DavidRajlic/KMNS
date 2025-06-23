4d8dde2dab1adc59a11315cc25c3b31bd325b3ff
DavidRajlic <rajlic.david@gmail.com>
2025-06-24 00:23:17 +0200

    small style changes and respossive design

--- frontend/src/app/tournament/matches/[id]/page.tsx
+++ frontend/src/app/tournament/matches/[id]/page.tsx
@@ -43,33 +43,30 @@ export default function MatchDetailsPage() {
 
   if (!match) {
     return (
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100 flex items-center justify-center">
-        <div className="bg-white rounded-xl p-8 shadow-lg border border-blue-200">
-          <div className="text-blue-600 font-semibold text-lg">
-            Nalaganje...
-          </div>
+      <div className="min-h-screen bg-gradient-to-br from-sky-50 via-blue-50 to-indigo-100 flex items-center justify-center">
+        <div className="bg-white/80 backdrop-blur-sm rounded-2xl p-8 shadow-xl border border-emerald-200">
+          <div className="text--700 font-semibold text-lg">Nalaganje...</div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100 p-3 ">
-      <div className=" max-w-4xl mx-auto">
+    <div className="min-h-screen bg-gradient-to-br from-sky-50 via-blue-50 to-indigo-100 p-3">
+      <div className="max-w-4xl mx-auto">
         {/* Match Details */}
-        <div className="bg-white  rounded-xl shadow-lg  border border-blue-200   pt-4">
-          <h1 className="sm:text-3xl text-xl max-[430px]:text-lg flex  justify-center pb-10 font-semibold text-blue-900">
-            {" "}
+        <div className="bg-gradient-to-r shadow-sm from-slate-50 to-gray-50  backdrop-blur-sm rounded-2xl shadow-xl border border-sky-200/50 pt-4">
+          <h1 className="sm:text-3xl text-xl max-[430px]:text-lg flex justify-center pb-10 font-bold text-slate-800">
             {match.team1_name} : {match.team2_name}
-            <span className="mx-2 text-black">
+            <span className="mx-2 text-sky-700 font-extrabold">
               {"   "}
-              {match.team1_goals} - {match.team2_goals}{" "}
+              {match.team1_goals} - {match.team2_goals}
             </span>
           </h1>
           <div className="grid grid-cols-2 gap-8">
             {/* Team 1 Column */}
             <div>
-              <h2 className="text-xl font-semibold text-blue-900 mb-6 text-center  pb-3"></h2>
+              <h2 className="text-xl font-semibold text-slate-700 mb-6 text-center pb-3"></h2>
               <div className="space-y-4">
                 <Section
                   title="Strelci"
@@ -91,7 +88,7 @@ export default function MatchDetailsPage() {
 
             {/* Team 2 Column */}
             <div>
-              <h2 className="text-xl font-semibold text-blue-900 mb-6 text-center  pb-3"></h2>
+              <h2 className="text-xl font-semibold text-slate-700 mb-6 text-center pb-3"></h2>
               <div className="space-y-4">
                 <Section
                   title="Strelci"
@@ -137,26 +134,28 @@ function Section({ items, type }: SectionProps) {
     console.log(items);
   }
   return (
-    <div className="rounded-lg">
+    <div className="rounded-xl bg-gradient-to-r from-slate-50 to-gray-50">
       <ul className="space-y-1 p-2">
         {items.map((p) => (
           <li
             key={p.player_id}
-            className="flex flex-wrap items-center  justify-between gap-x-0 gap-y-1 sm:text-base text-sm  sm:pb-1  pb-3"
+            className="flex flex-wrap items-center justify-between gap-x-0 gap-y-1 sm:text-base text-sm sm:pb-1 pb-3 px-2 py-1 rounded-lg hover:bg-white/60 transition-colors duration-200"
           >
-            <span className="font-medium text-blue-800 break-words ">
+            <span className="font-medium text-slate-700 break-words">
               <span className="inline max-[500px]:hidden">{p.player_name}</span>
-              <span className="hidden max-[500px]:inline ">
+              <span className="hidden max-[500px]:inline">
                 {shortenName(p.player_name)}
               </span>
             </span>
 
-            <span className="font-semibold text-xs sm:text-base text-sm  text-blue-900">
+            <span className="font-semibold text-xs sm:text-base text-sky-700">
               {type === "goals"
-                ? `${p.goals}x ⚽`
+                ? p.goals && p.goals < 2
+                  ? "⚽"
+                  : `${p.goals}x ⚽`
                 : type === "yellow"
-                ? `🟨`
-                : `🟥`}
+                ? "🟨"
+                : "🟥"}
             </span>
           </li>
         ))}
