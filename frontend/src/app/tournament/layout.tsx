6bf9509741f0b2cc242424001a759e0f1b8a5633
DavidRajlic <rajlic.david@gmail.com>
2025-06-25 01:00:27 +0200

    if admin show diffrent navbar

--- frontend/src/app/tournament/layout.tsx
+++ frontend/src/app/tournament/layout.tsx
@@ -1,13 +1,24 @@
 import Navbar from "@/app/components/Navbar";
+import Admin_Navbar  from "@/app/components/Admin_Navbar"
+import { cookies } from "next/headers";
 
-export default function TournamentLayout({
+ 
+
+export default async function TournamentLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+   const cookieStore = cookies();
+  const isAdmin = (await cookieStore).get("adminAccess")?.value === "true";
+
   return (
     <div>
-      <Navbar />
+      {isAdmin ? <Admin_Navbar />  :  <Navbar />
+
+      }
+      
       <main className="p-0">{children}</main>
     </div>
   );
